55edbecb86996201d442282580ab1bd10845b6c0
wankhede04 <wankhedevijay04@gmail.com>
2023-07-06 16:32:58 +0530

    Add register method

--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -9,4 +9,14 @@ import { IRegistration } from "./interfaces/IRegistration.sol";
  * @dev Registered participants can call the server maximum once every 12 seconds to report the weather.
  */
 contract Registration is IRegistration {
+    // Stores participants status
+    mapping(address => LifecycleStatus) public participants;
+
+    /// @inheritdoc IRegistration
+    function register() external virtual {
+        require(participants[msg.sender] == LifecycleStatus.Unregistered, "Already registered or resigned");
+
+        participants[msg.sender] = LifecycleStatus.Registered;
+        emit ParticipantRegistered(msg.sender);
+    }
 }
