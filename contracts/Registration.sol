f76549e48fe582e95585812e31281f78bf8803e9
wankhede04 <wankhedevijay04@gmail.com>
2023-07-06 16:32:13 +0530

    Add boiler contract for registration

---
+++ contracts/Registration.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.18;
+
+/**
+ * @title Registration Contract
+ * @notice This contract is used to perform registration, and resign.
+ * @dev Registered participants can call the server maximum once every 12 seconds to report the weather.
+ */
+contract Registration {
+}
