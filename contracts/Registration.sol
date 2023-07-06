805d8077348a057fa4f4dc830c233cf6b0a85666
wankhede04 <wankhedevijay04@gmail.com>
2023-07-06 16:32:37 +0530

    Inherit registration interface

--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.18;
 
+import { IRegistration } from "./interfaces/IRegistration.sol";
+
 /**
  * @title Registration Contract
  * @notice This contract is used to perform registration, and resign.
  * @dev Registered participants can call the server maximum once every 12 seconds to report the weather.
  */
-contract Registration {
+contract Registration is IRegistration {
 }
