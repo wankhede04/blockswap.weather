0d2d20ec76c2578f75e1315c9ebf531828ff6ec2
wankhede04 <wankhedevijay04@gmail.com>
2023-07-06 16:27:21 +0530

    Add arb-goerli network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,6 +13,19 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    arbitrumGoerli: {
+      url: `https://arb-goerli.g.alchemy.com/v2/${process.env.ARBITRUM_TESTNET_ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+      gasMultiplier: 1,
+      timeout: 36000000,
+    }
+  },
 };
 
 export default config;
